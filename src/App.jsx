cc267bcbef22262851b2befe94ef3674416470d2
thelifeisstrange <yogeshpk0123@gmail.com>
2025-02-21 20:22:32 +0530

    authpages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './styles/main.css';
 import './styles/animations.css';
 import { BrowserRouter, Routes, Route } from "react-router"
 import LoginPage from './components/AuthPages/LoginPage';
+import SignupPage from './components/AuthPages/SignupPage';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <Routes>
       <Route path="/" element={<ApplicationForm />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+
     </Routes>
     </BrowserRouter>
     </div>
---
+++ src/components/AuthPages/SignupPage.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+
+const SignupPage = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: ''
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  return (
+    <div className="container">
+      <div className="card animate-fade-in">
+        <h2 className="preview-title" style={{ textAlign: 'center' }}>
+          Signup
+        </h2>
+        <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '2rem' }}>
+          <div className="form-group">
+            <label className="form-label">Email</label>
+            <input
+              type="text"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              className="form-input"
+              placeholder="Enter your email id"
+            />
+          </div>
+
+          <div className="form-group">
+            <label className="form-label">Password</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              className="form-input"
+              placeholder="Enter your Password"
+            />
+          </div>
+          <div className="form-group">
+            <label className="form-label">Confirm Password</label>
+            <input
+              type="password"
+              name="cpassword"
+              value={formData.cpassword}
+              onChange={handleInputChange}
+              className="form-input"
+              placeholder="Re-enter Password"
+            />
+          </div>
+          <button 
+              className="button button-primary" 
+              onClick={() => console.log('Form submitted:', formData)} 
+              style={{ marginLeft: 'auto' }}
+            >
+              Submit Application
+            </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignupPage;
